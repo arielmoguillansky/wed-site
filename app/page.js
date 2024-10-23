4d813f13c3a30ba59e7bc9a67ff02cd987a784a0
kemba <ariel.moguillansky@42i.co>
2024-10-23 18:40:08 -0300

    v1

--- app/page.js
+++ app/page.js
@@ -1,101 +1,119 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [rsvpPayload, setRsvpPayload] = useState({
+    name: "",
+    restrictions: "",
+    willAttend: null,
+  });
+  const [sbumission, setSubmission] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+  const handleInputChange = (e) => {
+    setRsvpPayload((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleAssistance = (e) => {
+    setRsvpPayload((prevState) => ({
+      ...prevState,
+      willAttend: e,
+    }));
+  };
+
+  const handleSubmission = () => {
+    console.log("SUBMIT", rsvpPayload);
+    setSubmission(true);
+  };
+  return (
+    <main className="pb-12">
+      <section className="flex items-center justify-center w-full h-screen gap-x-4">
+        <span className="text-[72px]">Laura</span>
+        <span className="text-[72px]">Ariel</span>
+        [foto portada]
+      </section>
+      <section className="text-center">
+        <h2 className="uppercase max-w-[1024px] text-[60px] m-auto mb-4">
+          El momento llegó, nos estamos casando!
+        </h2>
+        <p className="max-w-[1024px] m-auto text-[24px] mb-6">
+          A 10 años de habernos visto por primera vez, despues de tantas
+          comidas, salidas, juegos y aventuras, decidimos finalmente compartir
+          una vida juntos y es nuestro deseo compartir este momento con vos!
+        </p>
+        <a href="#rsvp" className="bg-[#0d0804] p-4 text-[#fefae0] uppercase">
+          Vení a celebrar!
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+      <br />
+      <br />
+      <section id="rsvp" className="block m-auto w-[460px]">
+        <h2 className="text-center text-[48px] mb-6">RSVP</h2>
+        {sbumission ? (
+          <div>Tu confirmación quedó registrada!</div>
+        ) : (
+          <div className="flex flex-col gap-y-4">
+            <div>
+              <label className="block">Tu nombre</label>
+              <input
+                name="name"
+                type="text"
+                className="w-full p-4"
+                placeholder="Nombre completo"
+                onChange={(e) => handleInputChange(e)}
+              />
+            </div>
+            <div>
+              <label>Confirmás tu asistencia?</label>
+              <div className="flex w-full gap-x-4">
+                <div
+                  className={`w-1/2 p-4 bg-white border-4 cursor-pointer ${
+                    rsvpPayload.willAttend !== null && rsvpPayload.willAttend
+                      ? "border-[#CCD5AE]"
+                      : "border-white"
+                  }`}
+                  onClick={() => handleAssistance(true)}
+                >
+                  Con alegría confirmo!
+                </div>
+                <div
+                  className={`w-1/2 p-4 bg-white border-4 cursor-pointer ${
+                    rsvpPayload.willAttend !== null && !rsvpPayload.willAttend
+                      ? "border-[#CCD5AE]"
+                      : "border-white"
+                  }`}
+                  onClick={() => handleAssistance(false)}
+                >
+                  Con tristeza declino.
+                </div>
+              </div>
+            </div>
+            {rsvpPayload.willAttend && (
+              <div>
+                <label className="block">Restricciones alimentarias?</label>
+                <input
+                  name="restrictions"
+                  type="text"
+                  className="w-full p-4"
+                  placeholder="Nombre completo"
+                  onChange={(e) => handleInputChange(e)}
+                />
+              </div>
+            )}
+
+            <button
+              className="bg-[#CCD5AE] p-4 w-full"
+              disabled={!rsvpPayload.name || rsvpPayload.willAttend === null}
+              onClick={handleSubmission}
+            >
+              Enviar
+            </button>
+          </div>
+        )}
+      </section>
+    </main>
   );
 }
