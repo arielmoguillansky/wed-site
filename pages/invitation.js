2c9cbc10d5c5503efddb9ab156e0748fe70876a9
kemba <ariel.moguillansky@42i.co>
2024-12-19 09:03:05 -0500

    v3

--- pages/invitation.js
+++ pages/invitation.js
@@ -15,6 +15,10 @@ export default function Invitation() {
       name: "Rene",
       code: "rene-jose",
     },
+    {
+      name: "Alfredo",
+      code: "alfredo-gesell",
+    },
   ];
 
   useEffect(() => {
@@ -34,36 +38,38 @@ export default function Invitation() {
   }
 
   return (
-    <main className="p-12 space-y-8 bg-[#FFFDD0] text-[#4B3D26]">
+    <main className="p-12 md:space-y-16 space-y-8 max-w-[720px] m-auto">
       <div className="w-fit m-auto gap-y-6 text-[24px] md:text-[48px]">
         <p className="font-sans text-[18px] mb-2">Para</p>
         <p>{guest?.name}</p>
       </div>
-      <div className="font-sans text-center">
+      <div className="font-sans text-center md:text-[24px] text-[18px]">
         En el día que cambia nuestra vida para siempre, queremos que estés con
         nosotros y nos acompañes a celebrar nuestro amor y nuestra unión.
       </div>
       <div className="flex flex-col items-center justify-center gap-y-6 text-[24px] md:text-[48px]">
         <p>Laura Medrano</p>
-        <p className="mb-2">y</p>
+        <p className="mb-2 md:mb-12">y</p>
         <p>Ariel Moguillansky</p>
       </div>
-      <div className="flex justify-center font-sans text-[24px]">
-        Sábado 17 de mayo del 2025
+      <div className="font-sans text-[18px] md:text-[24px] text-center space-y-4">
+        <p>Sábado 17 de mayo del 2025</p>
+        <p>15:00 Hs</p>
       </div>
-      <div className="flex justify-center font-sans text-[24px]">15:00 Hs</div>
       <div className="flex flex-col items-center justify-center">
-        <p className="text-[24px] mb-4">Palacio Sans Souci</p>
-        <p className="font-sans">Paz 750, Victoria</p>
-        <p className="font-sans">Buenos Aires</p>
+        <p className="md:text-[32px] text-[20px] mb-4">Palacio Sans Souci</p>
+        <p className="font-sans text-[18px] md:text-[24px]">
+          Paz 750, Victoria
+        </p>
+        <p className="font-sans text-[18px] md:text-[24px]">Buenos Aires</p>
       </div>
       <div className="m-auto font-sans text-center">
         Mas información próximamente en nuestro sitio web.
       </div>
-      <div className="m-auto font-medium text-center">
+      <div className="m-auto font-medium text-center text-[18px]">
         <Link href="https://www.laurayariel.site">www.laurayariel.site</Link>
       </div>
-      <div className="text-center">¡Te esperamos!</div>
+      <div className="text-center text-[24px]">¡Te esperamos!</div>
     </main>
   );
 }
