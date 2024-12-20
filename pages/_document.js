71bea3ee0fe5c8a345f8fcaf7c22e28e11b216c9
kemba <ariel.moguillansky@42i.co>
2024-12-20 14:30:13 -0500

    v14

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Laura & Ariel</title>
+        <meta
+          name="description"
+          content="Vas a encontrar toda la información necesaria para el gran día."
+        />
+        <meta
+          property="og:title"
+          content="Nos casamos! Laura & Ariel"
+          key="title"
+        />
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
