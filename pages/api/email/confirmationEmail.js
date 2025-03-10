684ea1db3c60ce62430902c1d2d1d215cf7ed0c4
arielmoguillansky <arielmogui92@gmail.com>
2025-03-10 08:22:07 -0300

    resend api key

--- pages/api/email/confirmationEmail.js
+++ pages/api/email/confirmationEmail.js
@@ -4,23 +4,20 @@ import {
 } from "@/components/EmailTemplate";
 import { Resend } from "resend";
 
-const RESEND_API_KEY = process.env.NEXT_PUBLIC_RESEND_API_KEY;
+const RESEND_API_KEY = process.env.RESEND_API_KEY;
+const resend = new Resend(RESEND_API_KEY);
 
 const handler = async (req, res) => {
   const formData = JSON.parse(req.body);
 
   try {
-    const resend = new Resend(RESEND_API_KEY);
-
-    resend.emails.send({
-      from: "Boda Lau & Ari <onboarding@resend.dev>",
+    const { data, error } = await resend.emails.send({
+      from: "Boda Lau & Ari <vercel@resend.dev>",
       to: formData.email,
       subject: "Confirmación de asistencia",
-      react: formData.willAttend
-        ? ConfirmEmailTemplate({})
-        : VirtualEmailTemplate({}),
+      text: "HGOLA",
     });
-    console.log("~~~~~~ RESEND SENT~~~~~~~~~~");
+    console.log("~~~~~~ RESEND SENT~~~~~~~~~~", data);
 
     res.status(200).json({ message: "Email sent" });
   } catch (err) {
