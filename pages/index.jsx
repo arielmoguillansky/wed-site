a3549cf5f9eedfce16ac159a7b9c4c13731762c9
arielmoguillansky <arielmogui92@gmail.com>
2025-03-10 09:29:18 -0300

    latest updates v3

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,6 +15,20 @@ export default function Home({ weatherData }) {
   const headerRef = useRef(null); // Create a ref for the header
 
   useEffect(() => {
+    function setViewportHeight() {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+
+    setViewportHeight();
+    window.addEventListener("resize", setViewportHeight);
+
+    return () => {
+      window.removeEventListener("resize", setViewportHeight);
+    };
+  }, []);
+
+  useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
         setIsNavSticky(!entry.isIntersecting);
