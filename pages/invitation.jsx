db07bda3d77aedf9488dd5514a52996eea7b610c
arielmoguillansky <arielmogui92@gmail.com>
2025-03-12 18:31:22 -0300

    fix

--- pages/invitation.jsx
+++ pages/invitation.jsx
@@ -223,8 +223,6 @@ export default function Invitation() {
 }
 
 export async function getStaticProps(context) {
-  console.log(context);
-  
   return {
     props: {
       messages: (await import(`../lang/${context.locale}.json`)).default,
